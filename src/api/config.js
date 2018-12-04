dc00e442a476070fba44e6fc7d00af248e0ae152
macong <macong123>
2018-12-04 19:35:12 +0800

    config存储

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,9 @@
 let config = {
-  // site: 'webapi.fusion.tst.zanservice.com',//请求接口地址
-  site: '39.106.220.69:8092',//请求接口地址
-  // method: '/api/v1'//域名后面的接口参数
+  //请求接口地址
+  // site: 'webapi.fusion.tst.zanservice.com',
+  site: '39.106.220.69:8093',
+  //域名后面的接口参数
+  // method: '/api/v1'
   method: '/interface/dateService.do?returntype=2'
 }
 export const apiRoot = 'http://'.concat(config.site, config.method)
