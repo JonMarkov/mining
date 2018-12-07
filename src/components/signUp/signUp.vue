de5796c5b95df8504080e9a20d6b6f7bb5a8a412
macong <macong123>
2018-12-07 16:55:46 +0800

    注册页面已经做规则校验，未调试接口

--- src/components/signUp/signUp.vue
+++ src/components/signUp/signUp.vue
@@ -3,50 +3,50 @@
     <div class="register">
       <!--请输入手机号-->
       <div class="res-item">
-        <input type="tel" placeholder="必须输入本人银行卡绑定的手机号" class="input-item my-mobile">
+        <input type="tel" placeholder="必须输入本人银行卡绑定的手机号" class="input-item my-mobile" v-model="myMobile"/>
         <i class="res-icon am-icon-phone"></i>
       </div>
       <!--请输入短信验证码-->
       <div class="res-item">
-        <input type="text" placeholder="请输入短信验证码" class="input-item my-yanzheng">
+        <input type="text" placeholder="请输入短信验证码" class="input-item my-yanzheng" v-model="myYanzheng"/>
         <i class="res-icon am-icon-mobile"></i>
         <button type="button" class="yanzhengma">获取验证码</button>
       </div>
       <!--请输入姓名-->
       <div class="res-item">
-        <input type="text" placeholder="必须真实姓名且填写后不可更改" class="input-item my-name">
+        <input type="text" placeholder="必须真实姓名且填写后不可更改" class="input-item my-name" v-model="myName"/>
         <i class="res-icon am-icon-user"></i>
       </div>
       <!--请输入支付宝-->
       <div class="res-item">
-        <input type="text" placeholder="必须真实支付宝且填写后不可更改" class="input-item my-pay">
+        <input type="text" placeholder="必须真实支付宝且填写后不可更改" class="input-item my-pay" v-model="myPay"/>
         <i class="res-icon am-icon-paypal"></i>
       </div>
       <!--请输入登录密码-->
       <div class="res-item">
-        <input type="passWord" placeholder="请输入登录密码" class="input-item my-mima">
+        <input type="passWord" placeholder="请输入登录密码" class="input-item my-mima" v-model="myMima">
         <i class="res-icon am-icon-check-square-o"></i>
       </div>
       <!--请确认登录密码-->
       <div class="res-item">
-        <input type="passWord" placeholder="请再次输入登录密码" class="input-item my-repeatmima">
+        <input type="passWord" placeholder="请再次输入登录密码" class="input-item my-repeatmima" v-model="myRepeatmima">
         <i class="res-icon am-icon-check-circle-o"></i>
       </div>
       <!--请输入二级密码-->
       <div class="res-item">
-        <input type="passWord" placeholder="请输入二级密码" class="input-item my-two-mima">
+        <input type="passWord" placeholder="请输入二级密码" class="input-item my-two-mima" v-model="myTwoMima">
         <i class="res-icon am-icon-check-square-o"></i>
       </div>
       <!--请确认二级密码-->
       <div class="res-item">
-        <input type="passWord" placeholder="请再次输入二级密码" class="input-item my-two-repeatmima">
+        <input type="passWord" placeholder="请再次输入二级密码" class="input-item my-two-repeatmima" v-model="myTwoRepeatmima">
         <i class="res-icon am-icon-check-circle-o"></i>
       </div>
       <!--请输入推荐人手机号-->
-      <!--<div class="res-item">-->
-      <!--<input type="tel" placeholder="请输入推荐人手机号" class="input-item referee-mobile">-->
-      <!--<i class="res-icon am-icon-phone-square"></i>-->
-      <!--</div>-->
+      <div class="res-item">
+        <input type="tel" placeholder="请输入推荐人手机号" class="input-item referee-mobile" v-model="refereeMobile">
+        <i class="res-icon am-icon-phone-square"></i>
+      </div>
       <blockquote>
         <b>尊敬的EPC原始矿工请认真阅读以下规则：</b><br/>
         <span>【1】年龄范围18至70周岁，不用上传手持照片，注册帐号必须是本人银行卡绑定手机号码，系统自动审核并秒发放矿机。</span><br/>
@@ -89,11 +89,70 @@
       return {
         selected: 'signUp',
         topBatten: "注册新用户",
+        // 手机号
+        myMobile: '',
+        // 验证码
+        myYanzheng: '',
+        // 姓名
+        myName: '',
+        // 支付
+        myPay: '',
+        // 登录密码
+        myMima: '',
+        // 确认登录密码
+        myRepeatmima: '',
+        // 二级密码
+        myTwoMima: '',
+        // 确认二级密码
+        myTwoRepeatmima: '',
+        // 推荐人手机号
+        refereeMobile: ''
       }
     },
-    methods:{
+    methods: {
       // 请求接口-注册
       async goToSignUp() {
+        // 手机号判断
+        if (!this.myMobile || !(/^1[34578]\d{9}$/.test(this.myMobile))) {
+          alert('请输入正确的手机号')
+          return
+        }
+        // 验证码判断
+        if (!this.myYanzheng) {
+          alert('请输入验证码')
+          return
+        }
+        // 姓名输入判断
+        if(!this.myName){
+          alert('请填写姓名')
+          return
+        }
+        // 支付宝输入判断
+        if(!this.myPay||!(/^(?:\w+\.?)*\w+@(?:\w+\.)+\w+|\d{9,11}$/.test(this.myPay))){
+          alert('请填写正确的支付宝账号')
+          return
+        }
+        // 登录密码输入判断
+        if(!this.myMima||this.myMima.length>20||this.myMima.length<8||!(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$/.test(this.myMima))){
+          alert('登录密码必需大于8位小于20位且字母和数字组合')
+          return
+        }
+        // 确认登录密码输入判断
+        if(this.myRepeatmima != this.myMima){
+          alert('两次二级密码输入不一致')
+          return
+        }
+        // 二级密码输入判断
+        if(!this.myTwoMima||this.myMima.length>20||this.myMima.length<8||!(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$/.test(this.myMima))){
+          alert('二级密码必需大于8位小于20位且字母和数字组合')
+          return
+        }
+        // 确认二级密码判断输入
+        if(this.myTwoMima!=this.myTwoRepeatmima){
+          alert('两次二级密码输入不一致')
+          return
+        }
+        console.log(this.myMobile)
         let param = {
           service: 'login',
           login_name: this.login_name,
